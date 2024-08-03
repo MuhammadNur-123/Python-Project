56d85b1c9b3df4413373741d0b5c0d76661bcd6a
Nur Muhammad <141319241+MuhammadNur-123@users.noreply.github.com>
2024-08-03 14:54:21 +0600

    Add files via upload

---
+++ # Chessboard Patterrn.py
@@ -0,0 +1,9 @@
+# Chessboard Patterrn
+size = 20
+for i in range(size):
+    for j in range(size):
+        if (i +j)% 2 ==0:
+            print("!",end="")
+        else:
+            print("^",end="")
+    print()            
